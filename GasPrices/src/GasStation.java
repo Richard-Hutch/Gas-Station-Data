30b86f6581f92e34608e738f61b15435c31ef833
Richard Hutcheson <richardhutch80@hotmail.com>
2020-12-16 23:01:05 -0600

    Added station stats and extend stations into Canada

--- GasPrices/src/GasStation.java
+++ GasPrices/src/GasStation.java
@@ -1,4 +1,5 @@
-public class GasStation {
+
+public class GasStation implements Comparable<GasStation> {
 
     private String stationName = "";
     private String cost = "";
@@ -14,6 +15,7 @@ public class GasStation {
         amntReviews = reviews;
         stars = strs;
     }
+
 
     public String getStationName(){
         return stationName;
@@ -54,5 +56,35 @@ public class GasStation {
 
     public void setStars(String s) {
         stars = s;
+    }
+
+    @Override
+    public int compareTo(GasStation o) {
+        float tC = -1, oC = -1;
+        //check if gas station is Canadian and if is, remove the '¢' sign
+        if (this.cost.contains("¢")){
+            tC = Float.parseFloat(this.cost.substring(0, this.cost.length() - 1));
+            if (!o.cost.equals("---")){
+                oC = Float.parseFloat(o.cost.substring(0, o.cost.length() - 1));
+            }
+        }
+        else if (o.cost.contains("¢")){
+            oC = Float.parseFloat(o.cost.substring(0, o.cost.length() - 1));
+            if (!this.cost.equals("---")){
+                tC = Float.parseFloat(this.cost.substring(0, this.cost.length() - 1));
+            }
+        }
+        //check if there is a cost since we confirmed the station is not from Canada
+        else{
+            //if there is a cost, it is american, must remove dollar sign
+            if (!this.cost.equals("---")){
+                tC = Float.parseFloat(this.cost.substring(1, this.cost.length()));
+            }
+            if (!o.cost.equals("---")){
+                oC = Float.parseFloat(o.cost.substring(1, o.cost.length()));
+            }
+        }
+
+        return  (int)(oC * 100) - (int)(tC * 100);
     }
 }
--- GasPrices/src/Main.java
+++ GasPrices/src/Main.java
@@ -2,13 +2,16 @@
 Name: Richard Hutcheson
 Date: 12/15/2020
 
-Program finds the gas stations with the cheapest gas prices
-for the user's requested city
+Program finds gas stations from the user's requested
+city in the United States and Canada. Retrieves data
+such as name, address, rating, amnt of reviews, cost,
+amount of time since last updated, as well as the
 */
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner; //for user input
 
 public class Main {
@@ -28,7 +31,7 @@ public class Main {
             String line = "";
             //holds the indiviual gas stations and their information
             ArrayList<GasStation> stationList = new ArrayList<GasStation>();
-
+            String highestPrice = "0";
             //gets the HTML information for each gas station to break data up appropriately
             for (Element station : document.select(
                     "div.panel__panel___3Q2zW.panel__white___19KTz.colors__bg" +
@@ -61,6 +64,7 @@ public class Main {
                         }
                     }
                 }
+
                 //convert amount of stars to string
                 String amntStars = String.valueOf(tempAmntStars);
 
@@ -72,31 +76,68 @@ public class Main {
                 String address = station.select("div.GenericStationListItem__address___1VFQ3").text();
 
                 //get the cost of gas at the station
-                String cost = station.select("span.text__left___1iOw3.GenericStationListItem__price___3GpKP").text();
+                String cost = station.select("span.text__left___1iOw3.GenericSta" +
+                        "tionListItem__price___3GpKP").text();
+
+                //temp string var holds the current cost without dollar or cent sign
+                //so that it can be compared to the highest price
+                if (!cost.equals("---")){
+                    String temp = "";
+                    for (int i = 0; i < cost.length(); ++i){
+                        if (cost.charAt(i) != '$' && cost.charAt(i) != '¢'){
+                            temp += cost.charAt(i);
+                        }
+                    }
+                    if (Float.parseFloat(highestPrice) < Float.parseFloat(temp)){
+                        highestPrice = temp;
+                    }
+                }
 
                 //get the last update time at the station
                 String updateTime = station.select("span.ReportedBy__postedTime___J5H9Z").text();
 
                 //add Gas Station to list
                 stationList.add(new GasStation(name, cost, address, updateTime, amntReviews, amntStars));
-
             }
-            //display the gas stations and their information
-            for (int i = 0; i < stationList.size(); ++i){
-                System.out.println("Name:           " + stationList.get(i).getStationName());
-                System.out.println("Address:        " + stationList.get(i).getAddress());
-                System.out.print("Rating:         " + stationList.get(i).getStars() + "/5 stars");
-                System.out.println(" with " + stationList.get(i).getAmntReviews() + " reviews");
-                //the gas station did not have a price, therefore no last updated time either
-                if (stationList.get(i).getCost().equals("---")){
-                    System.out.println("Cost:           price unknown");
-                }else{
-                    System.out.print("Cost:           " + stationList.get(i).getCost());
-                    System.out.println(" (last updated " + stationList.get(i).getLastUpdated() + ")");
+
+            String lowestAndAvgPrice = document.select("span.text__lg___1S7OO.text__bold___1C6Z_." +
+                    "text__left___1iOw3.PriceTrends__priceHeader___fB9X9").text();
+            String [] lowAndAvgPriceArr = lowestAndAvgPrice.split(" ");
+            String lowestPrice = lowAndAvgPriceArr[0];
+            String avgPrice = lowAndAvgPriceArr[1];
+
+            //sort by cost so that greatest cost first and unknown last
+            Collections.sort(stationList);
+
+            System.out.println("-------------------------------------");
+            //City was not found
+            if (stationList.size() == 0){
+                System.out.println("Unknown city input. Please Try again.");
+            }else{
+                System.out.println("City's Lowest Price:  " + lowestPrice);
+                System.out.println("City's Average Price: " + avgPrice);
+                System.out.println("City's Highest Price: $" + highestPrice + "\n");
+
+                //display the gas stations and their information
+                for (int i = 0; i < stationList.size(); ++i){
+                    System.out.println("Name:           " + stationList.get(i).getStationName());
+                    System.out.println("Address:        " + stationList.get(i).getAddress());
+                    System.out.print("Rating:         " + stationList.get(i).getStars() + "/5 stars");
+                    System.out.println(" with " + stationList.get(i).getAmntReviews() + " reviews");
+                    //the gas station did not have a price, therefore no last updated time either
+                    if (stationList.get(i).getCost().equals("---")){
+                        System.out.println("Cost:           price unknown");
+                    }else{
+                        System.out.print("Cost:           " + stationList.get(i).getCost());
+                        System.out.println(" (last updated " + stationList.get(i).getLastUpdated() + ")");
+                    }
+                    //the gap between gas stations but wont place gap for the last station
+                    if (i < stationList.size() - 1){
+                        System.out.println();
+                    }
                 }
-                System.out.println();
             }
-
+            System.out.println("-------------------------------------");
         }
         //there was an error while processing the website or the contents of the website
         catch(Exception exc) {
